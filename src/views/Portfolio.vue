d9991b48c838ffe62ea49261e6ec4dd2d3811857
NastassiaD <degtyar.ana4@gmail.com>
2018-06-22 16:31:28 +0300

    portfolio-page sidebar

--- src/views/Portfolio.vue
+++ src/views/Portfolio.vue
@@ -5,7 +5,7 @@
 
   		<div class="portfolio-content__sidebar">
   			<ul class="portfolio-content__nav">
-  				<li class="portfolio-content__nav-item"><a href="#">Батик</a></li>
+  				<li class="portfolio-content__nav-item portfolio-content__nav-item--active"><a href="#">Батик</a></li>
   				<li class="portfolio-content__nav-item"><a href="#">Живопись</a></li>
   				<li class="portfolio-content__nav-item"><a href="#">Акварель</a></li>
   				<li class="portfolio-content__nav-item"><a href="#">Графика</a></li>
@@ -15,19 +15,33 @@
   			<div class="portfolio-content__contacts">
   				<img src="img/element01_top.svg">
   				<p>О возможности приобретения картин можно узнать по:</p>
-  				<div class="portfolio-content__phone"><img src="img/phone.svg"><p>+375 29 509 10 17</p></div>
-  				<div class="portfolio-content__mail"><img src="img/mail.svg"><p>degtyar.ana@mail.ru</p></div>
+  				<div class="portfolio-content__contacts-info">
+  					<div class="portfolio-content__phone"><img src="img/phone.svg"><p>+375 29 509 10 17</p></div>
+  					<div class="portfolio-content__mail"><img src="img/mail.svg"><p>degtyar.ana@mail.ru</p></div>
+  				</div>
   				<img src="img/element01_bottom.svg">
   			</div>
   		</div>
 
   		<div class="portfolio-content__gallery">
-  			<img src="img/test.jpg">
-  			<img src="img/test.jpg">
-  			<img src="img/test.jpg">
-  			<img src="img/test.jpg">
-  			<img src="img/test.jpg">
-  			<img src="img/test.jpg">
+  			<img src="img/test.jpg" class="portfolio-content__gallery-item">
+  			<img src="img/test.jpg" class="portfolio-content__gallery-item">
+  			<img src="img/test.jpg" class="portfolio-content__gallery-item">
+  			<img src="img/test.jpg" class="portfolio-content__gallery-item">
+  			<img src="img/test.jpg" class="portfolio-content__gallery-item">
+  			<img src="img/test.jpg" class="portfolio-content__gallery-item">
+  			<img src="img/test.jpg" class="portfolio-content__gallery-item">
+  			<img src="img/test.jpg" class="portfolio-content__gallery-item">
+  			<img src="img/test.jpg" class="portfolio-content__gallery-item">
+  			<img src="img/test.jpg" class="portfolio-content__gallery-item">
+  			<img src="img/test.jpg" class="portfolio-content__gallery-item">
+  			<img src="img/test.jpg" class="portfolio-content__gallery-item">
+  			<img src="img/test.jpg" class="portfolio-content__gallery-item">
+  			<img src="img/test.jpg" class="portfolio-content__gallery-item">
+  			<img src="img/test.jpg" class="portfolio-content__gallery-item">
+  			<img src="img/test.jpg" class="portfolio-content__gallery-item">
+  			<img src="img/test.jpg" class="portfolio-content__gallery-item">
+  			<img src="img/test.jpg" class="portfolio-content__gallery-item">
   		</div>
 
   	</div>
