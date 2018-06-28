24e6c5d1dac88e567ee1da9d063bf501a76cdc16
kyalex <st.zelenko@gmail.com>
2018-06-28 21:21:03 +0300

    fix

--- src/router.js
+++ src/router.js
@@ -2,11 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
 import Portfolio from './views/Portfolio.vue'
-<<<<<<< HEAD
-import portfolio_works from './components/portfolio_works'
-=======
 import Education from './views/Education.vue'
->>>>>>> d353f5b0d7b8cf957603d740542c6222a064cac3
 
 Vue.use(Router)
 
