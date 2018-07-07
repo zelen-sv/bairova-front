d515011bb12b9c58ea8d557e548ae22722b468fd
kyalex <st.zelenko@gmail.com>
2018-07-07 17:04:48 +0300

    fix carausel work component

--- src/components/PortfolioWork.vue
+++ src/components/PortfolioWork.vue
@@ -2,13 +2,13 @@
   <div class="carousel">
 
     <div class="carousel__image-part">
-    	<img src="img/homepage-oil3.jpg" class="carousel__image">
+    	<img :src="work.picture.url" class="carousel__image">
     	<div class="carousel__buttons"><button @click="$emit('previus')" class="carousel__button carousel__button-prev"></button><button @click="$emit('next')" class="carousel__button carousel__button-next"></button></div>
     </div>
 
     <div class="carousel__info-part">
-    	<p class="carousel__title"><img src="/img/dot.svg" class="carousel__dot">Название 01<img src="/img/dot.svg" class="carousel__dot"></p>
-    	<p class="carousel__technique">Техника: горячий батик, шелк</p>
+    	<p class="carousel__title"><img src="/img/dot.svg" class="carousel__dot">{{ work.title }}<img src="/img/dot.svg" class="carousel__dot"></p>
+    	<p class="carousel__technique">{{ work.technique }}</p>
     </div>
 
   </div>
