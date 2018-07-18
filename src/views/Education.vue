16967bdb5b8224e940bf05df2522146ee38f05ce
NastassiaD <degtyar.ana4@gmail.com>
2018-07-18 16:19:33 +0300

    teaching-page-markup-start

--- src/views/Education.vue
+++ src/views/Education.vue
@@ -1,3 +1,39 @@
 <template>
+	<main class="education-content">
+		<div class="education-content__wrapper">
 
+			<div class="education-content__sidebar sidebar">
+				<ul class="sidebar__nav">
+					<li class="sidebar__item router-link-exact-active router-link-active"><a href="#">Частные уроки</a></li>
+					<li class="sidebar__item"><a href="#">Студия "Флория"</a></li>
+					<li class="sidebar__item  router-link-exact-active router-link-active"><a href="#">Подготовка к поступлению</a></li>
+					<li class="sidebar__item"><a href="#">Работы учеников</a></li>
+				</ul>
+				<button class="sidebar__button request-button"><img src="/img/ic-palette.svg">Заявка на обучение</button>
+			</div>
+
+			<div class="education-content__information individual-page">
+				<div class="individual-page__introduction">
+					<div class="individual-page__main-image"><img src="/img/individual-main-image.jpg"></div>
+					<div class="individual-page__main-info">
+						<div class="individual-page__text">
+							<p>Lorem ipsum dolor sit amet, justo definitiones comprehensam mel ut. Velit gubergren ad qui. No veri movet euripidis vel. No impetus ornatus postulant pro, et agam phaedrum has, at vidit conceptam sit.</p>
+							<p>Lorem ipsum dolor sit amet, justo definitiones comprehensam mel ut. Velit gubergren ad qui. No veri movet euripidis vel. No impetus ornatus postulant pro, et agam phaedrum has, at vidit conceptam sit.</p>
+							<img src="/img/element02.svg">
+						</div>
+						<div class="individual-page__art-types">
+							<h3>Интересные направления:</h3>
+							<p class="individual-page__type">Батик (ручная роспись по шелку)</p>
+							<p class="individual-page__type">Живопись (масло, акрил)</p>
+							<p class="individual-page__type">Графика (карандаш, уголь)</p>
+							<p class="individual-page__type">Акварель</p>
+							<p class="individual-page__type">Китайская живопись</p>
+							<p class="individual-page__type">Арт-терапия (пескография,роспись по воде)</p>
+						</div>
+					</div>
+				</div>
+			</div>
+
+		</div>
+	</main>
 </template>
