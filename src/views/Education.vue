def18f9987b795dbf180fc2e2c5db0562f7d47b3
NastassiaD <degtyar.ana4@gmail.com>
2018-07-19 15:00:53 +0300

    teaching-page-markup

--- src/views/Education.vue
+++ src/views/Education.vue
@@ -9,12 +9,13 @@
 					<li class="sidebar__item  router-link-exact-active router-link-active"><a href="#">Подготовка к поступлению</a></li>
 					<li class="sidebar__item"><a href="#">Работы учеников</a></li>
 				</ul>
-				<button class="sidebar__button request-button"><img src="/img/ic-palette.svg">Заявка на обучение</button>
+				<button class="sidebar__button request-button"><img src="/img/ic-palette.svg">Обучение</button>
 			</div>
 
 			<div class="education-content__information individual-page">
+
 				<div class="individual-page__introduction">
-					<div class="individual-page__main-image"><img src="/img/individual-main-image.jpg"></div>
+					<div class="individual-page__main-image"></div>
 					<div class="individual-page__main-info">
 						<div class="individual-page__text">
 							<p>Lorem ipsum dolor sit amet, justo definitiones comprehensam mel ut. Velit gubergren ad qui. No veri movet euripidis vel. No impetus ornatus postulant pro, et agam phaedrum has, at vidit conceptam sit.</p>
@@ -22,14 +23,64 @@
 							<img src="/img/element02.svg">
 						</div>
 						<div class="individual-page__art-types">
-							<h3>Интересные направления:</h3>
-							<p class="individual-page__type">Батик (ручная роспись по шелку)</p>
-							<p class="individual-page__type">Живопись (масло, акрил)</p>
-							<p class="individual-page__type">Графика (карандаш, уголь)</p>
-							<p class="individual-page__type">Акварель</p>
-							<p class="individual-page__type">Китайская живопись</p>
-							<p class="individual-page__type">Арт-терапия (пескография,роспись по воде)</p>
+							<h4 class="individual-page__art-types-header">Интересные направления:</h4>
+							<ul class="individual-page__list">
+								<li class="individual-page__list-item">Батик (ручная роспись по шелку)</li>
+								<li class="individual-page__list-item">Живопись (масло, акрил)</li>
+								<li class="individual-page__list-item">Графика (карандаш, уголь)</li>
+								<li class="individual-page__list-item">Акварель</li>
+								<li class="individual-page__list-item">Китайская живопись</li>
+								<li class="individual-page__list-item">Арт-терапия (пескография,роспись по воде)</li>
+							</ul>
 						</div>
+					</div>
+				</div>
+
+				<div class="individual-page__firstblock artblock">
+					<div class="artblock__image"></div>
+					<div class="artblock__description">
+						<img src="/img/batiktitle.svg" class="artblock__title">
+						<p class="artblock__text">Lorem ipsum dolor sit amet, justo definitiones comprehensam mel ut. Velit gubergren ad qui. No veri movet euripidis vel. No impetus ornatus postulant pro, et agam phaedrum has, at vidit conceptam sit.</p>
+					</div>
+				</div>
+
+				<div class="individual-page__secondblock artblock">
+					<div class="artblock__image"></div>
+					<div class="artblock__description">
+						<img src="/img/paintingtitle.svg" class="artblock__title">
+						<p class="artblock__text">Lorem ipsum dolor sit amet, justo definitiones comprehensam mel ut. Velit gubergren ad qui. No veri movet euripidis vel. No impetus ornatus postulant pro, et agam phaedrum has, at vidit conceptam sit.</p>
+					</div>
+				</div>
+
+				<div class="individual-page__thirdblock artblock">
+					<div class="artblock__image"></div>
+					<div class="artblock__description">
+						<img src="/img/graphictitle.svg" class="artblock__title">
+						<p class="artblock__text">Lorem ipsum dolor sit amet, justo definitiones comprehensam mel ut. Velit gubergren ad qui. No veri movet euripidis vel. No impetus ornatus postulant pro, et agam phaedrum has, at vidit conceptam sit.</p>
+					</div>
+				</div>
+
+				<div class="individual-page__fourthblock artblock">
+					<div class="artblock__image"></div>
+					<div class="artblock__description">
+						<img src="/img/watercolortitle.svg" class="artblock__title">
+						<p class="artblock__text">Lorem ipsum dolor sit amet, justo definitiones comprehensam mel ut. Velit gubergren ad qui. No veri movet euripidis vel. No impetus ornatus postulant pro, et agam phaedrum has, at vidit conceptam sit.</p>
+					</div>
+				</div>
+
+				<div class="individual-page__fifthblock artblock">
+					<div class="artblock__image"></div>
+					<div class="artblock__description">
+						<img src="/img/chinesetitle.svg" class="artblock__title">
+						<p class="artblock__text">Lorem ipsum dolor sit amet, justo definitiones comprehensam mel ut. Velit gubergren ad qui. No veri movet euripidis vel. No impetus ornatus postulant pro, et agam phaedrum has, at vidit conceptam sit.</p>
+					</div>
+				</div>
+
+				<div class="individual-page__sixthblock artblock">
+					<div class="artblock__image"></div>
+					<div class="artblock__description">
+						<img src="/img/therapytitle.svg" class="artblock__title">
+						<p class="artblock__text">Lorem ipsum dolor sit amet, justo definitiones comprehensam mel ut. Velit gubergren ad qui. No veri movet euripidis vel. No impetus ornatus postulant pro, et agam phaedrum has, at vidit conceptam sit.</p>
 					</div>
 				</div>
 			</div>
