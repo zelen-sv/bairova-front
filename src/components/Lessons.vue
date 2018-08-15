599ce4b9ad8404c6a112f6b29b14e9a3ea65fd6a
NastassiaD <degtyar.ana4@gmail.com>
2018-08-15 15:29:24 +0300

    information adding in lessons.vue

--- src/components/Lessons.vue
+++ src/components/Lessons.vue
@@ -10,14 +10,14 @@
           <img src="/img/element02.svg">
         </div>
         <div class="individual-page__art-types art-types">
-          <h4 class="art-types__header">Интересные направления:</h4>
+          <h4 class="art-types__header">Направления обучения:</h4>
           <ul class="art-types__list">
-            <li class="art-types__list-item">Батик (ручная роспись по шелку)</li>
-            <li class="art-types__list-item">Живопись (масло, акрил)</li>
-            <li class="art-types__list-item">Графика (карандаш, уголь)</li>
-            <li class="art-types__list-item">Акварель</li>
-            <li class="art-types__list-item">Китайская живопись</li>
-            <li class="art-types__list-item">Арт-терапия (пескография,роспись по воде)</li>
+            <li class="art-types__list-item">Основы композиции</li>
+            <li class="art-types__list-item">Живопись</li>
+            <li class="art-types__list-item">Графика</li>
+            <li class="art-types__list-item">Батик</li>
+            <li class="art-types__list-item">Арт-терапия</li>
+            <li class="art-types__list-item">Интуитивное рисование</li>
           </ul>
         </div>
       </div>
@@ -26,8 +26,8 @@
     <div class="individual-page__firstblock individual-page__block artblock">
       <div class="artblock__image"></div>
       <div class="artblock__description">
-        <img src="/img/batiktitle.svg" class="artblock__title">
-        <p class="artblock__text">Lorem ipsum dolor sit amet, justo definitiones comprehensam mel ut. Velit gubergren ad qui. No veri movet euripidis vel. No impetus ornatus postulant pro, et agam phaedrum has, at vidit conceptam sit.</p>
+        <img src="/img/compotitle.svg" class="artblock__title">
+        <p class="artblock__text">Композиция является фундаментом в изобразительном искусстве. Это умение выразить главное, видеть целое. И как результат – создание произведения, картины, что даёт ощущение сопричастности к искусству и развивает ученика как творческую личность.</p>
       </div>
     </div>
 
@@ -35,7 +35,7 @@
       <div class="artblock__image"></div>
       <div class="artblock__description">
         <img src="/img/paintingtitle.svg" class="artblock__title">
-        <p class="artblock__text">Lorem ipsum dolor sit amet, justo definitiones comprehensam mel ut. Velit gubergren ad qui. No veri movet euripidis vel. No impetus ornatus postulant pro, et agam phaedrum has, at vidit conceptam sit.</p>
+        <p class="artblock__text">Что такое живопись? Берём краски, кисточки, бумагу или холст. Добавляем фантазию и немного смелости. И вот ты уже начинающий художник – живописец. А дальше рисуем то, что любим, что нас окружает. И получаются картины в разных жанрах живописи: портрет, пейзаж, натюрморт. Стоит только начать, и ты убедишься, что это интересно и увлекательно.</p>
       </div>
     </div>
 
@@ -43,31 +43,31 @@
       <div class="artblock__image"></div>
       <div class="artblock__description">
         <img src="/img/graphictitle.svg" class="artblock__title">
-        <p class="artblock__text">Lorem ipsum dolor sit amet, justo definitiones comprehensam mel ut. Velit gubergren ad qui. No veri movet euripidis vel. No impetus ornatus postulant pro, et agam phaedrum has, at vidit conceptam sit.</p>
+        <p class="artblock__text">Точка, линия, пятно, штрих – вот из чего состоит твой карандашный рисунок. И это уже графика, вид изобразительного искусства, включающий рисунок и печатные художественные изображения.В современных условиях, благодаря компьютерной графике и полиграфии, она получила широкое распространение ( книжные иллюстрации, товарные знаки, плакаты и др.)</p>
       </div>
     </div>
 
     <div class="individual-page__fourthblock individual-page__block artblock">
       <div class="artblock__image"></div>
       <div class="artblock__description">
-        <img src="/img/watercolortitle.svg" class="artblock__title">
-        <p class="artblock__text">Lorem ipsum dolor sit amet, justo definitiones comprehensam mel ut. Velit gubergren ad qui. No veri movet euripidis vel. No impetus ornatus postulant pro, et agam phaedrum has, at vidit conceptam sit.</p>
+        <img src="/img/batiktitle.svg" class="artblock__title">
+        <p class="artblock__text">Батик – это очень древний восточный способ украшения ткани, который сохранил свои традиции. А в наше время появились все необходимые инструменты и материалы, поэтому овладеть техникой ручной росписи ткани может каждый. Это процесс творческой медитации, в результате которой создаются неповторимые картины, элементы интерьера и даже авторская одежда!</p>
       </div>
     </div>
 
     <div class="individual-page__fifthblock individual-page__block artblock">
       <div class="artblock__image"></div>
       <div class="artblock__description">
-        <img src="/img/chinesetitle.svg" class="artblock__title">
-        <p class="artblock__text">Lorem ipsum dolor sit amet, justo definitiones comprehensam mel ut. Velit gubergren ad qui. No veri movet euripidis vel. No impetus ornatus postulant pro, et agam phaedrum has, at vidit conceptam sit.</p>
+        <img src="/img/therapytitle.svg" class="artblock__title">
+        <p class="artblock__text">В студии арт-терапия представлена молодым видом искусства – пескографией, которая успокаивает, расслабляет, а также развивает внимание, пространственное мышление и фантазию. А ещё песочная живопись может стать способом раскрытия художественного таланта.</p>
       </div>
     </div>
 
     <div class="individual-page__sixthblock individual-page__block artblock">
       <div class="artblock__image"></div>
       <div class="artblock__description">
-        <img src="/img/therapytitle.svg" class="artblock__title">
-        <p class="artblock__text">Lorem ipsum dolor sit amet, justo definitiones comprehensam mel ut. Velit gubergren ad qui. No veri movet euripidis vel. No impetus ornatus postulant pro, et agam phaedrum has, at vidit conceptam sit.</p>
+        <img src="/img/intuitiontitle.svg" class="artblock__title">
+        <p class="artblock__text">Это выражение своих чувств и внутренних ощущений любыми техниками и художественными материалами. Создавая полотно своего эмоционального внутреннего мира, осознаёшь свою уникальность.</p>
       </div>
     </div>
   </div>
