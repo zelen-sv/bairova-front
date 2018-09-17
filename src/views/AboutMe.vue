0d09d5cfa3015068e81ff4338736e75150ee83d1
zelen-sv <34160181+zelen-sv@users.noreply.github.com>
2018-09-17 11:57:54 +0300

    About page (#4)
    * about-page markup
    
    * bug fix
    
    * change-main-photo-properties

--- src/views/AboutMe.vue
+++ src/views/AboutMe.vue
@@ -1,1 +1,28 @@
-<template><div>О художнике</div></template>
+<template>
+	<main class="about-content">
+		<div class="about-content__wrapper main-wrapper">
+
+			<div class="about-content__main-text">
+				<img src="/img/artist-name-violet.svg" class="about-content__name">
+				<p class="about-content__city">Беларусь, Минск</p>
+				<div class="about-content__accent-items">
+					<p class="about-content__accent-item">Высшее художественное и педагогическое образование</p>
+					<p class="about-content__accent-item">Дизайнер, художник, педагог высшей категории</p>
+					<p class="about-content__accent-item">Член Белорусского союза дизайнеров</p>
+			  </div>
+				<p>Работает в разных творческих направлениях, участвует в художественных выставках, арт - проектах и благотворительных акциях.</p>
+				<p>Сотрудничество с белорусским ТВ (цикл передач для детей «Не зевай», «Своя компания») и каналом «Мир» (мастер – классы по дизайну).</p>
+				<p>Более 20 лет педагогической деятельности. С 2006г. руководит творческой студией «Флория» при Учреждении образования «Республиканский центр экологии и краеведения».</p>
+				<p>Основные виды творческой деятельности: картины, текстиль в интерьере (шторы, ширмы, светильники и др.), роспись одежды, иллюстрации книг.</p>
+				<p>Среди основных работ: картины и другие произведения находятся в частных коллекциях Беларуси, России, Италии и Латвии.</p>
+			</div>
+			<div class="about-content__main-photo"><img src="img/painter-photo.jpg"></div>
+			<div class="about-content__bird"><img src="img/bird.svg"></div>
+			<div class="about-content__rewards">
+				<img src="img/reward01-2.jpg">
+				<img src="img/reward02-2.jpg">
+				<img src="img/reward03-2.jpg">
+			</div>
+		</div>
+	</main>
+</template>
