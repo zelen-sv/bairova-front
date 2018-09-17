dbba89007b3b4a0dfb91113c3d36d34fcf9b6fcb
kyalex <st.zelenko@gmail.com>
2018-09-17 17:22:35 +0300

    error page

--- src/router.js
+++ src/router.js
@@ -5,6 +5,7 @@ import Portfolio from './views/Portfolio.vue'
 import Education from './views/Education.vue'
 import AboutMe from './views/AboutMe.vue'
 import Contacts from './views/Contacts.vue'
+import Error404 from './views/Error404.vue'
 
 import PortfolioWorks from './components/PortfolioWorks.vue'
 
@@ -80,7 +81,8 @@ export default new Router({
       path: '/contacts',
       name: 'contacts',
       component: Contacts
-    }
+    },
+    { path: '*', component: Error404 }
   ],
   scrollBehavior (to, from, savedPosition) {
     if (to.hash) {
