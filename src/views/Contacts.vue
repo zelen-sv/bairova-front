2bd45d7206320f5c3af5ac5f9ced2609df17499f
NastassiaD <degtyar.ana4@gmail.com>
2018-09-19 15:15:13 +0300

    contacts page markup, actual links addition

--- src/views/Contacts.vue
+++ src/views/Contacts.vue
@@ -1,1 +1,20 @@
-<template><div>Контакты</div></template>
+<template>
+	<main class="contacts-content" id="contacts">
+		<div class="contacts-content__wrapper main-wrapper">
+
+			<div class="contacts-content__info-block artblock">
+				<div class="contacts-content__info">
+					<div class="contacts-content__phone">
+						<img src="/img/phone.svg">
+						<p class="contacts-content__phone-number">+375 29 365 21 10<br>(Viber, Telegram)</p>
+					</div>
+					<div class="contacts-content__mail">
+						<img src="/img/mail.svg">
+						<p>bairova.info@mail.ru</p>
+					</div>
+				</div>
+			</div>
+
+		</div>
+	</main>
+</template>
