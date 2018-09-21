be3e2abbb4412ef8e72e5de3e45385ad46f10f3c
zelen-sv <34160181+zelen-sv@users.noreply.github.com>
2018-09-21 14:59:01 +0300

    page events coming soon (#6)

--- src/views/Events.vue
+++ src/views/Events.vue
@@ -1,1 +1,11 @@
-<template><div id="events">События</div></template>
+<template>
+	<main class="events-content" id="events">
+		<div class="events-content__wrapper main-wrapper">
+
+			<div class="events-content__coming-soon">
+				<img src="/img/picture404.png">
+			</div>
+
+		</div>
+	</main>
+</template>
